7d30a880beb2f9fec62123eae670a0a835b63925
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2026-02-21 01:53:46 +0200

    edit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const API_BASE_URL = import.meta.env.PROD
-  ? "/api" // Netlify proxy
+  ? "" // same origin (relative paths starting with /api)
   : import.meta.env.VITE_API_BASE_URL || "http://50.6.228.16:4000";
 
 const api = axios.create({
